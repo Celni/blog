2749bf92fdb9135b569326b6d9041c286e5c4a65
Олег <celni@yandex.ru>
2019-11-13 21:29:41 +0300

    Удалил работу с https (пусть nginx этим занимается)

--- src/Blog.Web/Startup.cs
+++ src/Blog.Web/Startup.cs
@@ -60,11 +60,8 @@ namespace Blog.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
