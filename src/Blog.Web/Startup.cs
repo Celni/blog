5150d0744aa412eabbd3a9b82008b9fdc79f1432
Олег <celni@yandex.ru>
2019-11-13 21:54:44 +0300

    Add .dockerignore

--- src/Blog.Web/Startup.cs
+++ src/Blog.Web/Startup.cs
@@ -33,6 +33,7 @@ namespace Blog.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<IdentityContext>(options =>
                 options.UseNpgsql(
                     Configuration.GetConnectionString("IdentityConnection")));
